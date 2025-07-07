41233a40b640601f56885cb1681c7d804ec3860a
Chirag Adve <chiragadve4@gmail.com>
2025-07-07 21:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (24.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       int rows = matrix.size();
-       int cols = matrix[0].size();
-       int mark = -987656789876;
 
-       for(int i = 0; i < rows; i++) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        vector<int> rowsarr(rows,0);
+        vector<int> colsarr(cols,0);
+
+        for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(matrix[i][j] == 0) {
-                    for(int k = 0; k < rows; k++) {
-                        if(matrix[k][j] != 0)
-                        matrix[k][j] = mark;
-                    }
-                    for(int l = 0; l < cols; l++) {
-                        if(matrix[i][l] != 0)
-                        matrix[i][l] = mark;
-                    }
-                   
+                    rowsarr[i] = 1;
+                    colsarr[j] = 1;
                 }
             }
-       }
-       for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            if(matrix[i][j] == mark) {
-                matrix[i][j] = 0;
+        }
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++) {
+                if(rowsarr[i] == 1 || colsarr[j] == 1){
+                    matrix[i][j] = 0;
+                }
             }
         }
-       }
     }
 };
