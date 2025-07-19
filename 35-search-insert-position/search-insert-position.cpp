776d716d91dba1c8961c33de1a3b65fe58659ed5
Chirag Adve <chiragadve4@gmail.com>
2025-07-19 23:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (42.23%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int result;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] >= target) {
+                result = i;
+                break;
+            }else{
+                result = nums.size();
+            }
+        }
+    return result;
+    }
+};
