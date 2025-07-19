bf96bc21fd697b1b808cae3d26c611d64a5a8d21
Chirag Adve <chiragadve4@gmail.com>
2025-07-19 15:49:17 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (51.41%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -5,7 +5,10 @@ public:
         int j = s.size() - 1;
 
         while(j > i) {
-            swap(s[i],s[j]);
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
             i++;
             j--;
         }
