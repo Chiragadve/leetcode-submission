b36782fe2cb03144337a7abce8f53c6a41aab06c
Chirag Adve <chiragadve4@gmail.com>
2025-08-22 15:13:10 +0530

    Time: 251 ms (76.04%) | Memory: 133.4 MB (72.52%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -3,7 +3,10 @@ public:
     int minimumArea(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        // fill the max with -1 and min with n and m so that it doesn't hold the garbage
+        // and can be used for comparision 
         int maxrow = -1, maxcol = -1, minrow = n,mincol = m;
+        // traversing the entire 2d matrix
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 1){
@@ -14,6 +17,7 @@ public:
                 }
             }
         }
+        // computing the final row and col
         int row = maxrow - minrow + 1;
         int col = maxcol - mincol + 1;
     return row * col;
