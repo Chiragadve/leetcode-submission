9819174952b49fd0c9d961be62518d219696e18c
Chirag Adve <chiragadve4@gmail.com>
2025-09-09 20:18:54 +0530

    Time: 517 ms (97.85%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,2 +1,2 @@
--- Write your PostgreSQL query statement below
-SELECT tweet_id from Tweets WHERE LENGTH(content) > 15;
+# Write your MySQL query statement below
+select tweet_id from Tweets where length(content) > 15;
