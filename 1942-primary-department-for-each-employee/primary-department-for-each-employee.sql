54f74a8f94e94367047ee8f8c425e9f22f73230a
Chirag Adve <chiragadve4@gmail.com>
2025-09-16 16:48:49 +0530

    Time: 559 ms (44.43%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-select employee_id,department_id from (
-select *, count(*) over (partition by employee_id) as dept_count from employee) as t
-where primary_flag = 'Y' or dept_count = 1;
+select employee_id, department_id from Employee 
+where primary_flag = 'Y'
+union
+select employee_id, department_id from Employee 
+group by employee_id
+having count(employee_id) = 1
