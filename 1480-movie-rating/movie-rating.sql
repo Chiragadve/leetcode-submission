100d89107b96e4c0b88c56ff09d3c618bd79f35f
Chirag Adve <chiragadve4@gmail.com>
2025-09-22 16:37:37 +0530

    Time: 1107 ms (88.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+(select name as results from MovieRating m join Users u on m.user_id = u.user_id
+group by name order by count(rating) desc, name limit 1)
+
+union all
+
+(select title as results from MovieRating join Movies using(movie_id)
+where extract(year_month from created_at) = 202002
+group by title
+order by avg(rating) desc, title limit 1
+)
