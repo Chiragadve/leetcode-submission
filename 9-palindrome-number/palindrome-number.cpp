fcf37a45a0bbd38169e081280a7e47d341fde865
Chirag Adve <chiragadve4@gmail.com>
2025-12-26 18:28:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (40.99%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int revNum = 0;
-        int original = x;
+        int reverse = 0;
+        int org = x;
         if (x < 0)
             return false;
-
         while (x != 0) {
             int lastdigit = x % 10;
-             if (revNum > INT_MAX / 10 ||
-               (revNum == INT_MAX / 10 && lastdigit > 7)) {
-                return false;
+
+            if (reverse > INT_MAX / 10 ||
+                (reverse == INT_MAX / 10 && lastdigit > 7)) {
+                return 0;
             }
-            revNum = revNum * 10 + lastdigit;
+
+            if (reverse < INT_MIN / 10 ||
+                (reverse == INT_MIN / 10 && lastdigit < -8)) {
+                return 0;
+            }
+
+            reverse = reverse * 10 + lastdigit;
             x /= 10;
         }
-
-        if (revNum == original) {
+        if (reverse == org) {
             return true;
         }
         return false;
