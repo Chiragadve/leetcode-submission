0ba6689579ba0d6acfe74f6901fce77ed81b5d74
Chirag Adve <chiragadve4@gmail.com>
2026-01-01 22:57:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (56.44%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -10,21 +10,12 @@ public:
     }
 
     string gcdOfStrings(string str1, string str2) {
-        int i = 0;
-        int j = 0;
-        string s = "";
         int num1 = str1.size();
         int num2 = str2.size();
 
         if (str1 + str2 != str2 + str1)
             return "";
-        while (i < str1.size() && j < str2.size()) {
-            if (str1[i] == str2[j]) {
-                s += str1[i];
-            }
-            i++;
-            j++;
-        }
-        return s.substr(0,gcd(num1,num2));
+
+        return str1.substr(0,gcd(num1,num2));
     }
 };
