ebd5111d1f4d75020e488fbeb73671cfc185ade7
Chirag Adve <chiragadve4@gmail.com>
2026-03-16 22:17:29 +0530

    Time: 11 ms (33.93%) | Memory: 23.3 MB (15.40%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,13 +4,10 @@ class Solution:
         left = 0
         right = len(clean) - 1
 
-
         while left < right:
             if clean[left] != clean[right]:
                 return False
             left += 1
             right -= 1
         return True
-
-
         
