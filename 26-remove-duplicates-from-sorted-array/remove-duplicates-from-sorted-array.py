783a92872172a621b4bb0ca43746b5ff9615b869
Chirag Adve <chiragadve4@gmail.com>
2026-04-07 18:17:39 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (9.33%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -9,16 +9,8 @@ class Solution:
             if(nums[i] != nums[j]):
                 i+=1
                 nums[i] = nums[j]
-            j+=1     
-
+            j+=1
         return i+1
 
-
-
-
-        
-        
-
-        
-        
+    
         
