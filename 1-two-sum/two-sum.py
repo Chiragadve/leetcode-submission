fa05318b297a54ebb5a571b49d37e424c8bc8a72
Chirag Adve <chiragadve4@gmail.com>
2026-04-15 13:27:25 +0530

    Time: 3 ms (52.86%) | Memory: 20.5 MB (47.21%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(len(nums)):
             compliment = target - nums[i]
             if compliment in map:
-                return [map[compliment],i]
+                return [map.get(compliment),i]
             else:
                 map[nums[i]] = i
 
