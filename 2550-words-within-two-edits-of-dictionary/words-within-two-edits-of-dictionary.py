e8b92aeb5147eb78f1dd830887d3601b96d6609b
Chirag Adve <chiragadve4@gmail.com>
2026-04-22 16:02:35 +0530

    Time: 7 ms (96.57%) | Memory: 19.3 MB (96.08%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        result = []
+        for query in queries:
+            for word in dictionary:
+                diff = 0
+                for i in range(len(query)):
+                    if query[i] != word[i]:
+                        diff += 1
+                    if diff > 2:
+                        break
+
+                if diff <= 2:
+                    result.append(query)
+                    break
+        return result
